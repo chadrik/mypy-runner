3aec70ef971a19bcf98792fc4ef5b746476e488a
Chad Dombrova <chadrik@gmail.com>
2019-03-31 21:58:13 -0700

    Basic setup.py and readme

---
+++ README.rst
@@ -0,0 +1,7 @@
+# mypy-runner
+
+Wraps mypy and adds the following features:
+
+- colorize output
+- filter errors
+- convert errors to warnings
---
+++ setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+import os.path
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with open(os.path.join(HERE, *parts)) as f:
+        return f.read()
+
+
+setup(
+    name="mypyrun",
+    version="0.1.0",
+    author="Chad Dombrova",
+    description="Run mypy with options to filter errors and colorize output",
+    long_description=read("README.rst"),
+    license="MIT",
+    keywords=["mypy", "typing", "pep484", "annotations"],
+    url="https://github.com/chadrik/mypy-runner",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['mypyrun=mypyrun:main'],
+    },
+    install_requires=[
+        "termcolor",
+        "mypy",
+    ],
+    extras_require={
+        "tests": [
+            "coverage",
+            "pytest==3.6.2",
+            "tox==2.7.0",
+        ],
+    },
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
