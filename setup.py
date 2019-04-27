afdbb129cdc2536f67d53797166b16cbb0041bf9
Chad Dombrova <chadrik@gmail.com>
2019-04-26 18:02:16 -0700

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*parts):
 
 setup(
     name="mypy-runner",
-    version="0.1.6",
+    version="0.1.7",
     author="Chad Dombrova",
     description="Run mypy with options to filter errors and colorize output",
     long_description=read("README.rst"),
