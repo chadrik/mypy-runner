25045bbf3b784c2c352769685996f9ffeeecc5fe
Chad Dombrova <chadrik@gmail.com>
2019-06-10 11:39:12 -0700

    Add --version flag

--- mypyrun.py
+++ mypyrun.py
@@ -18,6 +18,8 @@ except ImportError:
 if False:
     from typing import *
 
+__version__ = "0.1.8"
+
 # adapted from mypy:
 CONFIG_FILE = 'mypyrun.ini'
 SHARED_CONFIG_FILES = ('mypy.ini', 'setup.cfg')
@@ -769,6 +771,8 @@ def get_parser():
             inverse, action='store_false',
             dest=dest, help=argparse.SUPPRESS)
 
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s {version}'.format(version=__version__))
     parser.add_argument("--list",
                         help="List error codes",
                         action="store_true")
